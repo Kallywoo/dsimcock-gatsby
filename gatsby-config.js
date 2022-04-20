e6102cc307fd066669e4f9deb6b728d46fe4bd27
Kalvin <47543827+Kallywoo@users.noreply.github.com>
2022-04-20 02:37:21 +0100

    update s3 workflow
    * updated deployment script to s3
    * updated action trigger
    * updated creds retrieval
    * added region
    * removed commented code and only push on merge to main
    * added email service variables to env file creation
    
    Co-authored-by: Michael Cockcroft <cocymsc@hotmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,16 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
+const targetAddress = new URL(
+  process.env.TARGET_ADDRESS || `http://localhost:8000`
+);
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "D Simcock and Son | Builders Doncaster",
-    description: "D Simcock and Son Builders Doncaster | Builders in Doncaster since 1986 | Building & Industrial Contractors, Doncaster, South Yorkshire | Tel 01302 710302 Mobile 07836 527331 | Email dsimcock@aol.com",
+    description:
+      "D Simcock and Son Builders Doncaster | Builders in Doncaster since 1986 | Building & Industrial Contractors, Doncaster, South Yorkshire | Tel 01302 710302 Mobile 07836 527331 | Email dsimcock@aol.com",
   },
   plugins: [
     {
@@ -27,6 +32,25 @@ module.exports = {
         path: "./src/images/",
       },
       __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: process.env.TARGET_BUCKET_NAME || "fake-bucket",
+        region: process.env.AWS_REGION,
+        protocol: targetAddress.protocol.slice(0, -1),
+        hostname: targetAddress.hostname,
+        acl: null,
+        params: {
+          // In case you want to add any custom content types: https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/custom-content-type.md
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: targetAddress.href.slice(0, -1),
+      },
     },
     "gatsby-plugin-react-helmet",
   ],
